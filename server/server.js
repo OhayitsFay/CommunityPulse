9306f11b770461af222844cc8ff59b08c835dae0
Adedeji234501 <azeezadedeji1k@gmail.com>
2026-03-19 08:00:39 -0700

    backend server setup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config()
 
@@ -8,11 +9,11 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("CommunityPulse API running")
+  res.send("CommunityPulse API is running")
 })
 
 const PORT = 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log("Server running on port " + PORT)
 })
